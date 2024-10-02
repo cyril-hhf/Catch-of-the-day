430d456cc735c2b920b5cffdfc1643ca6b750714
cyril-hhf <cyrilhhf@gmail.com>
2024-10-02 18:10:08 +0800

    Added onSubmit to StorePickers so user can route to App with value input in StorePickers

--- catch-of-the-day/src/components/Storepicker.js
+++ catch-of-the-day/src/components/Storepicker.js
@@ -2,12 +2,26 @@ import React from 'react';
 import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component {
+
+    myInput = React.createRef()
+    goToStore = event => {
+        //1. Stop the form from submitting
+        event.preventDefault();
+        //2. get the text from that input
+        const storeName = this.myInput.current.value;
+        //3. Change the page to /store/whatever-they-attend
+        this.props.history.push(`/store/${storeName}`)
+    }
     render() {
         return (
             <React.Fragment>
-            <form className="store-selector">
+            <form className="store-selector" onSubmit={this.goToStore}>
                 <h2>Please Enter A Store</h2>
-                <input type="text" required placeholder='Store Name' defaultValue={getFunName()}>
+                <input
+                    type="text"
+                    ref={this.myInput}
+                    required placeholder='Store Name'
+                    defaultValue={getFunName()}>
                 </input>
                 <button type="submit">Visit Store</button>
             </form>
